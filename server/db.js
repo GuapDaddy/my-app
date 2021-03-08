1116035b45862877147d71f65605e88e5e85f9e8
Guapo <kyle.miller.pokemonpro@gmail.com>
2021-03-07 21:56:23 -0500

    fixed DB

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const path = require ('path')
-const dbPath= path.resolve(__dirname, 'db/database.sqlite')
+const dbPath = path.resolve(__dirname, 'db/database.sqlite')
 
 const knex = require('knex')({
     client: 'sqlite3',
@@ -13,28 +13,28 @@ knex.schema
     .hasTable('ingredient')
         .then((exists) => {
             if (!exists) { 
-                return knex.shema.createTable('ingredient', (table) => {
+                return knex.schema.createTable ('ingredient', (table) => {
                     table.increments('id').primary()
                     table.string('ingredient')
                     table.integer('quantity')
             })
             .then(() => {
-                console.log('Table\'Ingredient\'created')
+                console.log('Table\'ingredient\'created')
             })
             .catch((error) => {
-                console.log('there was an error creating table: ${error}')
+                console.log(`there was an error creating table: ${error}`)
             })
         }
     })
     .then(() => {
-    consol.log('done')
+    console.log('done')
     })
     .catch((error) => { 
-        console.error('There was an error setting up the database: ${error}')
+        console.error(`There was an error setting up the database: ${error}`)
     })
 
 knex.select('*').from('ingredient')
     .then(data => console.log('data:', data))
-    .catch(err=>console.log(err))
+    .catch(err => console.log(err))
 
 module.exports = knex
